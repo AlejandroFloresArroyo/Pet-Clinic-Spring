91b53a5de45ac3c64ffb7fc79acdf60d4e3f396d
Alejandro Flores <itt.alejandro.flores@hotmail.com>
2021-02-11 19:43:47 -0600

    Refactor to common crud interface

---
+++ Pet-clinic-data/src/main/java/com/thefilmsvault/petclinic/services/CrudService.java
@@ -0,0 +1,13 @@
+package com.thefilmsvault.petclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+    T findByID(ID id);
+    T save(T object);
+    void delete(T object);
+    void deleteById(T object);
+
+}
